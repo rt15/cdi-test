0b700b3305b6c2835275abbe1930d25607661354
Boissard Bruno <bboissard@axway.com>
2017-10-16 16:01:00 +0200

    Add workaround

--- src/test/java/fr/rt15/MyTest.java
+++ src/test/java/fr/rt15/MyTest.java
@@ -1,5 +1,7 @@
 package fr.rt15;
 
+import javax.inject.Inject;
+
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,8 +9,15 @@ import org.junit.runner.RunWith;
 @RunWith(CdiTestRunner.class)
 public class MyTest {
 
+	@Inject
+	private MyBean myBean;
+
 	@Test
 	public void test() throws Exception {
+
+		// Ugly workaround: uncomment next line.
+		// this.myBean.foo();
+
 		MyThread myThread = new MyThread();
 		myThread.start();
 		myThread.join();
